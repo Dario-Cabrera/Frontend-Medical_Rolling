d76563e128139db5490fefb57f5e31c8f6339afa
Cristhian Sosa <cristhiansosazurita@gmail.com>
2024-04-15 16:24:07 -0300

    add:protected route

---
+++ src/ProtectedRouteDoctor.jsx
@@ -0,0 +1,10 @@
+import { doctorAuth } from "./Context/DoctorContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+export const ProtectedRouteDoctor = () => {
+  const { loadingDoctor, isAuthenticatedDoctor } = doctorAuth();
+  console.log(loadingDoctor, isAuthenticatedDoctor);
+  if (loadingDoctor) return <h1>loadingDoctor...</h1>;
+  if (!loadingDoctor && !isAuthenticatedDoctor) return <Navigate to="/loginDoctor/" />;
+  return <Outlet />;
+};
--- src/api/doctor.auth.js
+++ src/api/doctor.auth.js
@@ -1,10 +1,31 @@
-import axios from "axios";
+/* eslint-disable no-useless-catch */
+import axios from "./axios";
 
-const API ="http://localhost:3001/api"
+export const registerRequestDoctor = (doctor) =>
+  axios.post(`/createdoctor/`, doctor);
 
+export const loginRequestDoctor = (doctor) =>
+  axios.post(`/logindoctor`, doctor);
 
+export const varityDoctorRequest = async (data) => {
+  // eslint-disable-next-line no-useless-catch
+  /*     console.log("Estoy en docto.auth",data)
+   */ try {
+    const res = await axios.post("/verifydoctor", data);
+    /*       console.log("Estoy saliendo del await",res.data)
+     */ return res.data; //VERIFICAR QUE ESTA PASANDO AQUI
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const registerRequestDoctor = (doctor) => axios.post(`${API}/createdoctor/`, doctor)
-
-export const loginRequestDoctor = (doctor) => axios.post(`${API}/logindoctor`, doctor)
-
+export const varityDoctorRequest2 = async (doctorId) => {
+  try {
+    const res = await axios.post("/verifydoctor", {
+      dataDoctor: { id: doctorId },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
