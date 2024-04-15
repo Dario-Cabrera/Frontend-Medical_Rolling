33ee3180e0d1b5ad01060fe0a5983cee19ebf5c4
Dario-Cabrera01~ <dario.cabrera17@gmail.com>
2024-04-14 23:40:39 -0300

    Se realizan las paginas de ERROR404 y ContactUs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import {Navbar} from './Components/Wrappers/Navbar';
-import {Footer} from './Components/Wrappers/Footer'
-import {ContactPage} from './Views/ContactPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./Components/Wrappers/Navbar";
+import { Footer } from "./Components/Wrappers/Footer";
+import Error404 from "./Views/ERROR404";
+import AboutUs from "./Views/AboutUs";
+import Homepage from "./Views/Homepage";
+import ContactForm from "./Views/ContactUs";
 
 function App() {
-
-  return(
-      <>
-      <Navbar/>
-      <ContactPage/>
-      <Footer/>
-      </>
-     
-  ) 
+  return (
+    <Router>
+      <div>
+        {/* Envuelve tus rutas en un div o un fragmento */}
+        <Navbar />
+        <Routes>
+          <Route path="/error" element={<Error404 />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/contactus" element={<ContactForm />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Views/ContactUs.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setShowModal(true);
+    reset();
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className="relative flex items-top justify-center min-h-screen bg-w sm:items-center sm:pt-0 font-roboto">
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="mt-8 overflow-hidden">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="p-8 bg-gray-100 sm:rounded-lg">
+              <h1 className="text-4xl sm:text-5xl text-c font-extrabold tracking-tight">Contacto</h1>
+              <p className="text-normal text-lg sm:text-2xl font-medium text-c mt-4">Rellena el formulario para ponerte en contacto con nosotros</p>
+              <div className="flex flex-col mt-8 text-c">
+                <h6 className="footer-title text-ts">Direccion y horarios</h6>
+                <p className="">General Paz 656</p>
+                <p className="">Lunes a Viernes: 08:00 a 16:00</p>
+                <p className="">Sábados: 08:00 a 12:00</p>
+                <h6 className="footer-title mt-6 text-ts">Teléfonos</h6>
+                <p className="">Whatsapp: +54-0381-1234567</p>
+                <p className="">Tel: +54-0381-4123456</p>
+                <h6 className="footer-title mt-6 text-ts">Redes Sociales</h6>
+                <div className="flex mt-2">
+                  <a className="mr-2" href="#">
+                    <img src="https://i.ibb.co/WGr2GVn/facebook-5968973-NEGRO.png" width="29" height="29" className="img-fluid cursor-pointer" />
+                  </a>
+                  <a className="mr-2" href="#">
+                    <img src="https://i.ibb.co/xDwCBvp/instagram-5968982-NEGRO.png" width="29" height="29" className="img-fluid cursor-pointer" />
+                  </a>
+                  <a href="#">
+                    <img src="https://i.ibb.co/V22ntPY/whatsapp-5969035-NEGRO.png" width="29" height="29" className="img-fluid cursor-pointer" />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="p-8 bg-gray-100 sm:rounded-lg">
+              <div className="flex flex-col">
+                <label htmlFor="name" className="hidden">
+                  Nombre Completo
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: "Este campo es obligatorio" })}
+                  id="name"
+                  placeholder="Nombre Completo"
+                  className="w-full mt-2 py-2 px-4 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-ts focus:outline-none"
+                />
+                {errors.name && <span className="text-red-500">{errors.name.message}</span>}
+              </div>
+
+              <div className="flex flex-col mt-4">
+                <label htmlFor="email" className="hidden">
+                  Correo Electrónico
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: "Este campo es obligatorio" })}
+                  id="email"
+                  placeholder="Correo Electrónico"
+                  className="w-full mt-2 py-2 px-4 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-ts focus:outline-none"
+                />
+                {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+              </div>
+
+              <div className="flex flex-col mt-4">
+                <label htmlFor="tel" className="hidden">
+                  Número de Teléfono
+                </label>
+                <input
+                  type="tel"
+                  {...register("tel", { required: "Este campo es obligatorio", minLength: { value: 8, message: "El número de teléfono debe tener al menos 8 caracteres" } })}
+                  id="tel"
+                  placeholder="Número de Teléfono"
+                  className="w-full mt-2 py-2 px-4 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-ts focus:outline-none"
+                />
+                {errors.tel && <span className="text-red-500">{errors.tel.message}</span>}
+              </div>
+
+              <div className="flex flex-col mt-4">
+                <label htmlFor="message" className="hidden">
+                  Mensaje
+                </label>
+                <textarea
+                  {...register("message", { required: "Este campo es obligatorio" })}
+                  id="message"
+                  placeholder="Mensaje"
+                  className="w-full min-h-[200px] max-h-[300px] mt-2 py-2 px-4 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-ts focus:outline-none resize-none"
+                />
+                {errors.message && <span className="text-red-500">{errors.message.message}</span>}
+              </div>
+
+              <button type="submit" className="w-full mt-6 bg-hb hover:bg-ts text-w hover:text-c font-bold py-3 px-6 rounded-lg hover:bg-ts transition ease-in-out duration-300">
+                Enviar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal */}
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" onClick={closeModal}>
+          <div className="bg-white p-8 max-w-md rounded-lg" onClick={(e) => e.stopPropagation()}>
+            <h2 className="text-xl text-c font-bold mb-4">Mensaje enviado correctamente.</h2>
+            <div className="flex justify-center">
+              <button className="bg-hb hover:bg-ts text-w hover:text-c font-bold py-1 px-2 rounded inline-flex items-center" onClick={closeModal}>
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ContactForm;
