00ef4d5f07b43e903e710c84f18f47d968afbf23
Cristhian Sosa <cristhiansosazurita@gmail.com>
2024-04-15 16:40:07 -0300

    add:appointment form

---
+++ src/Context/AppointmentContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState } from "react";
+import {
+  createRequestAppointment,
+  getAppointmentsRequest,
+} from "../api/appointment.auth.js";
+
+const AppointmentContext = createContext();
+
+export const useAppointment = () => {
+  const context = useContext(AppointmentContext);
+  if (!context) {
+    throw new Error("useAppointment must be used within a AppointmentProvider");
+  }
+
+  return context;
+};
+
+export const AppointmentProvider = ({ children }) => {
+  const [appointments, setAppointments] = useState([]);
+
+  const createAppointment = async (appointment) => {
+    console.log(appointment);
+    const res = await createRequestAppointment(appointment);
+    console.log(res);
+  };
+
+  const getAppointments = async () => {
+    try {
+      const res = await getAppointmentsRequest();
+
+      setAppointments(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AppointmentContext.Provider
+      value={{ appointments, createAppointment, getAppointments }}
+    >
+      {children}
+    </AppointmentContext.Provider>
+  );
+};
