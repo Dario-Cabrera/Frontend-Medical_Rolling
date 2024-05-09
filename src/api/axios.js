2748f9fff485a0a0a94896e2e0c81434990c3d2b
Dario-Cabrera01~ <dario.cabrera17@gmail.com>
2024-05-08 21:15:20 -0300

    Se realiza commit para deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://backend-medical-rolling-e2u6evw2k.vercel.app/api",
   withCredentials: true,
 });
 
