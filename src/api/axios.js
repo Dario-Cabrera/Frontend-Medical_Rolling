ca4f817f7e06c1b0f122efff3492fa73aaa90c77
Dario-Cabrera01~ <dario.cabrera17@gmail.com>
2024-05-08 22:42:33 -0300

    Prueba deploy 5

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://backend-medical-rolling-39nneqg01.vercel.app/api",
+  baseURL: "https://backend-medical-rolling.vercel.app/api",
   withCredentials: true,
 });
 
